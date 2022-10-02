be647ac598fddc446f0ea7739c0a73e260ef2580
Artan <artanpl03@gmail.com>
2022-10-02 12:02:11 -0400

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,30 @@
-
+import Login from './Login';
+import SignUp from './SignUp';
+import Deposit from './Deposit';
+import Profile from './Profile';
 import './App.css';
+import { Routes, Route} from "react-router-dom"
+import {useState} from 'react'
 
 function App() {
+const [name, setName] = useState('')
+const [amount, setAmount] = useState('')
+
+const handleNameSubmit = (event) =>{
+  setName(event.target.value)
+}
+const handleAmountSubmit = (event) =>{
+  setAmount(event.target.value)
+}
+
   return (
     <div className="App">
-      hello
+      <Routes>
+        <Route path = '/' element={<Login/>} />
+        <Route path = '/SignUp' element={<SignUp handleNameSubmit={handleNameSubmit}/>} />
+        <Route path = '/deposit' element={<Deposit handleAmountSubmit={handleAmountSubmit}/>} />
+        <Route path = '/profile' element={<Profile />} />
+      </Routes>
     </div>
   );
 }
---
+++ frontend/src/Deposit.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import './Deposit.css'
+import { Link } from 'react-router-dom'
+
+
+
+function Deposit() {
+  return (
+    <div className='container'>
+        <div className='smallerContainer'>
+            <div>
+                Deposit amount
+            </div>
+            <div>
+            {/* <label for="price" class="block text-sm font-medium text-gray-700">Price</label> */}
+            <div class="relative mt-1 rounded-md shadow-sm">
+    <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+      <span class="text-gray-500 sm:text-sm">$</span>
+    </div>
+    <input type="text" name="price" id="price" class="block w-full rounded-md border-gray-300 pl-7 pr-12 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" placeholder="0.00"/>
+  
+            </div>
+            </div>
+            <div>
+                Deposit from  TD CHECKINGS (..0628)
+            </div>
+            <div>Deposit will be processedby 11PM ET and will be reflected in your vault by the day
+
+            </div>
+            <Link to='/profile'>
+            <button className='button'>Continue</button>
+            </Link>
+        </div>
+    </div>
+  )
+}
+
+export default Deposit
+
